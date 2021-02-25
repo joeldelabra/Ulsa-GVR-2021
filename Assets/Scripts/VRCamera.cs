111d3e9a3dd7aa7d0b969b00650551f2801566c5
joeldelabra <joeldelabra@gmail.com>
2021-02-24 20:12:51 -0700

    cardboard 2021 GVR2021

---
+++ Assets/Scripts/VRCamera.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRCamera : MonoBehaviour
+{
+
+    [SerializeField]
+    Color rayColor = Color.magenta;
+
+    [SerializeField, Range(0.1f, 100f)]
+    float rayDistance = 5f;
+
+    [SerializeField]
+    LayerMask rayLayerDetection;
+
+    RaycastHit hit;
+
+    [SerializeField]
+    Transform reticleTrs;
+
+    [SerializeField]
+    Vector3 initialScale;
+
+    bool objectTouched;
+
+
+    void Update()
+    {
+        if(Physics.Raycast(transform.position, transform.forward, out hit, rayDistance, rayLayerDetection))
+        {
+
+            if(!objectTouched) objectTouched = true;
+            if(!reticleTrs) return;
+            reticleTrs.position = hit.point;
+            reticleTrs.localScale = initialScale * hit.distance;
+            //reticleTrs.LookAt(hit.normal);
+
+        } else {
+
+            if(objectTouched){
+                
+                objectTouched = false;
+                reticleTrs.localScale = initialScale;
+                reticleTrs.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1);
+            
+            }
+        }
+
+    }
+
+    void OnDrawGizmosSelected() 
+    {
+        Gizmos.color = rayColor;
+        Gizmos.DrawRay(transform.position, transform.forward * rayDistance);
+    }
+
+}
